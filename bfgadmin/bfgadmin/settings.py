ebe53caa50b4636a7ad93a6110b81248e2c3b99e
AleksandrTan <rumych@list.ru>
2017-04-25 10:51:49 +0300

    Static add

--- bfgadmin/bfgadmin/settings.py
+++ bfgadmin/bfgadmin/settings.py
@@ -86,6 +86,17 @@ DATABASES = {
         'PASSWORD': 'root',
         #'HOST': '127.0.0.1',
         #'PORT': '5432',
+    },
+
+    'mainusers': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bfgadmin',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432',
     }
 }
 
