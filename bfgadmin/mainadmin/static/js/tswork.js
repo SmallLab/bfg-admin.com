caba7763b1c28ac271ed7e1e8edbb30578eea099
AleksandrTan <rumych@list.ru>
2017-05-16 21:49:24 +0300

    Users work

--- bfgadmin/mainadmin/static/js/tswork.js
+++ bfgadmin/mainadmin/static/js/tswork.js
@@ -1,26 +1,5 @@
 $(document).ready( function(){
 /*
-* Changes 'is_active' field for type sentense data
-* */
-  $('button[type=button]').click(function () {
-      if($(this).attr('data_info')){
-          var that = $(this)
-          $.get(
-              "/typesent/ajaxts/isactive/"+$(this).attr("data_info")+"/",
-              onAjaxSuccess
-            );
-            function onAjaxSuccess(data)
-            {
-              if (data.status == true){
-                  that.text("True").removeClass("btn-danger").addClass("btn-success")
-              }
-              else{
-                  that.text("False").removeClass("btn-success").addClass("btn-danger")
-              }
-            }
-      }
-  });
-  /*
 * Add new type sentense data
 * */
   $('#add_new_type').click(function () {
@@ -43,11 +22,40 @@ $(document).ready( function(){
             function onAjaxSuccess(data)
             {
               if (data.status){
-
+                    var new_type = '<tr><td></td><td class="text-center">'+new_name+'</td><td class="text-center"><button type="button"'+
+                                    'class="btn btn-sm btn-danger" data_info = "'+data.id+'">False</button></td><td class="text-center">'+
+                                    ''+new_link+'</td><td class="text-center"><a type="button" data_delete="delete" id="'+data.id+'"'+
+                                    'class="btn btn-sm btn-danger" href="/typesent/deleteset/'+data.id+'/">Delete</a></td>';
+                    $('#type_s_list').append(new_type);
               }
             }
 
   });
+/*
+* Changes 'is_active' field for type sentense data
+* */
+  $('td').on('click', 'button', function (e) {
+      console.log('3000$');
+      e.preventDefault();
+      return false;
+      if($(this).attr('data_info')){
+          var that = $(this)
+          $.get(
+              "/typesent/ajaxts/isactive/"+$(this).attr("data_info")+"/",
+              onAjaxIsActive
+            );
+            function onAjaxIsActive(data)
+            {
+              if (data.status == true){
+                  that.text("True").removeClass("btn-danger").addClass("btn-success")
+              }
+              else{
+                  that.text("False").removeClass("btn-success").addClass("btn-danger")
+              }
+            }
+      }
+  });
+
 
 /*
 * Delete type sentense data
@@ -56,16 +64,18 @@ $(document).ready( function(){
         $('#is_deleted_ts').modal();
         $('#delete_type').attr('data_id_type', $(this).attr('href'));
         return false;
-    })
+    });
+
     $('#cancel_type').click(function () {
         $('#is_deleted_ts').modal('hide');
         $('#delete_type').attr('data_id_type', '');
         return false;
-    })
+    });
+
 
     $('#delete_type').click(function () {
         $('#is_deleted_ts').modal('hide');
         var url = $(this).attr('data_id_type');
         $(location).attr('href',url);
-    })
+    });
 });
